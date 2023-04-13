fe91ac243d794abf0c77fb9122e1daf5bdf6daaf
Fisher Sun <f.sun@wustl.edu>
2023-04-13 13:32:23 -0500

    Move contents of LoggerTest into ThreadTest, add LoggerTest

--- tests/LoggerTest.cpp
+++ tests/LoggerTest.cpp
@@ -3,8 +3,6 @@
 //
 
 #include <thread>
-#include <future>
-#include <cstdint>
 
 #include "gtest/gtest.h"
 
@@ -13,23 +11,9 @@
 Logger *Logger::instance{nullptr};
 std::mutex Logger::mutex;
 
-uintptr_t threadedLoggerAddress() {
-    // Following code emulates slow initialization.
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    Logger *logger = Logger::getInstance();
-    return reinterpret_cast<std::uintptr_t>(logger);
+TEST(LoggerTest, isSingleton) {
+    Logger *logger0 = Logger::getInstance();
+    Logger *logger1 = Logger::getInstance();
+
+    ASSERT_EQ(logger0, logger1) << "Multiple calls to getInstance should return the same logger";
 }
-
-TEST(LoggerTest, isThreadSafe) {
-    std::future<uintptr_t> future0 = std::async(&threadedLoggerAddress);
-    std::future<uintptr_t> future1 = std::async(&threadedLoggerAddress);
-    std::future<uintptr_t> future2 = std::async(&threadedLoggerAddress);
-
-    auto address0 = future0.get();
-    auto address1 = future1.get();
-    auto address2 = future2.get();
-
-    ASSERT_EQ(address0, address1) << "Loggers in multiple threads should be the same";
-    ASSERT_EQ(address0, address2) << "Loggers in multiple threads should be the same";
-}
-
---
+++ tests/ThreadTest.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Fisher on 4/13/23.
+//
+
+#include <thread>
+#include <future>
+#include <cstdint>
+
+#include "gtest/gtest.h"
+
+#include "logger/Logger.h"
+
+Logger *Logger::instance{nullptr};
+std::mutex Logger::mutex;
+
+uintptr_t threadedLoggerAddress() {
+    // Following code emulates slow initialization.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    Logger *logger = Logger::getInstance();
+    return reinterpret_cast<std::uintptr_t>(logger);
+}
+
+TEST(ThreadTest, isThreadSafe) {
+    std::future<uintptr_t> future0 = std::async(&threadedLoggerAddress);
+    std::future<uintptr_t> future1 = std::async(&threadedLoggerAddress);
+    std::future<uintptr_t> future2 = std::async(&threadedLoggerAddress);
+
+    auto address0 = future0.get();
+    auto address1 = future1.get();
+    auto address2 = future2.get();
+
+    ASSERT_EQ(address0, address1) << "Loggers in multiple threads should be the same";
+    ASSERT_EQ(address0, address2) << "Loggers in multiple threads should be the same";
+}
+
