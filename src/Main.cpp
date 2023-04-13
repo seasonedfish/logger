dc84251777684fbc083ab6420bd8ad982f53271f
Fisher Sun <f.sun@wustl.edu>
2023-04-13 13:00:54 -0500

    Add threads to check if singleton worked properly

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,8 @@
 // Created by Fisher on 4/13/23.
 //
 
+#include <thread>
+#include <iostream>
 #include "logger/Logger.h"
 
 // i hate c++ bruh is there really no better way of fixing this
@@ -9,6 +11,23 @@
 Logger *Logger::instance{nullptr};
 std::mutex Logger::mutex;
 
+void threadFoo() {
+    // Following code emulates slow initialization.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    Logger* logger = Logger::getInstance();
+    std::cout << logger << "\n";
+}
+
+void threadBar() {
+    // Following code emulates slow initialization.
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    Logger* logger = Logger::getInstance();
+    std::cout << logger << "\n";
+}
+
 int main() {
-    Logger *logger = Logger::getInstance();
+    std::thread t1(threadFoo);
+    std::thread t2(threadBar);
+    t1.join();
+    t2.join();
 }
