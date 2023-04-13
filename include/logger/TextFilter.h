3c8662795c02ab1176183e27b4f1bdb72b1b7d8b
Fisher Sun <f.sun@wustl.edu>
2023-04-13 14:26:34 -0500

    Implement TextFilter

---
+++ include/logger/TextFilter.h
@@ -0,0 +1,21 @@
+//
+// Created by Fisher on 4/13/23.
+//
+
+#ifndef LOGGER_TEXTFILTER_H
+#define LOGGER_TEXTFILTER_H
+
+#include <string>
+
+#include "AbstractFilter.h"
+
+class TextFilter : AbstractFilter {
+private:
+    std::string pattern;
+public:
+    explicit TextFilter(std::string pattern);
+
+    bool match(const std::string &text) override;
+};
+
+#endif //LOGGER_TEXTFILTER_H
